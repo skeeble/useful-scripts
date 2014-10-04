e5a88ed699cf04caa0a750dee31e8fc243809c8e
Lewis Cowles <lewiscowles@me.com>
2014-10-04 02:37:42 +0100

    added automatic pw generate

--- root/create_db.sh
+++ root/create_db.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+PW=`./gen_pw.sh 16`
 echo "CREATE DATABASE $1_db IF NOT EXISTS;" >> $1.sql
-echo "CREATE USER '$1user'@'%' IDENTIFIED BY '$pw';" >> $1.sql
+echo "CREATE USER '$1user'@'%' IDENTIFIED BY '$PW';" >> $1.sql
 echo "GRANT ALL PRIVILEGES ON $1_db.* TO '$1user'@'%';" >> $1.sql
 echo "FLUSH PRIVILEGES;" >> $1.sql
 echo `cat $1.sql`
