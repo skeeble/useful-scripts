a092950b761ac0fc9fe7a36558f307a898fe4e25
Lewis Cowles <lewiscowles@me.com>
2014-10-04 02:38:43 +0100

    Update create_db.sh

--- root/create_db.sh
+++ root/create_db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PW=`./gen_pw.sh 16`
+PW=`gen_pw.sh 16`
 echo "CREATE DATABASE $1_db IF NOT EXISTS;" >> $1.sql
 echo "CREATE USER '$1user'@'%' IDENTIFIED BY '$PW';" >> $1.sql
 echo "GRANT ALL PRIVILEGES ON $1_db.* TO '$1user'@'%';" >> $1.sql
